37956a27f0f77f0277e510787fb485da5a996de7
piloni <josalimaxwell@gmail.com>
2026-03-18 14:55:52 +0300

    requirements

--- app.py
+++ app.py
@@ -932,4 +932,5 @@ def repeat_task(log_id):
 # ---------------- Run ----------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
